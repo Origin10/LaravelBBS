b80fba8608d0224f9f01e72df5706cd2c1cdf4f0
origin10 <w.crows@gmail.com>
2018-03-26 07:12:35 +0000

    Topic index page

---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,13 @@
+@if (count($topics))
+<ul class="media-list">@foreach ($topics as $topic)
+  <li class="media">
+    <div class="media-left"><a href="{{ route('users.show', [$topic->user_id]) }}"><img style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}" class="media-object img-thumbnail"/></a></div>
+    <div class="media-body">
+      <div class="media-heading"><a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">{{ $topic->title }}</a><a href="{{ route('topics.show', [$topic->id]) }}" class="pull-right"><span class="badge"> {{ $topic->reply_count }} </span></a></div>
+      <div class="media-body meta"><a href="#" title="{{ $topic->category->name }}"><span aria-hidden="true" class="glyphicon glyphicon-folder-open"></span>{{ $topic->category->name }}</a><span> • </span><a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}"><span aria-hidden="true" class="glyphicon glyphicon-user">{{ $topic->user->name }}</span></a><span> • </span><span aria-hidden="true" class="glyphicon glyphicon-time"></span><span title="最後活躍於" class="timeago">{{ $topic->updated_at->diffForHumans() }}</span></div>
+    </div>
+  </li>@if ( ! $loop->last)
+  <hr/>@endif
+  @endforeach
+</ul>@else
+<div class="empty-block">戰無數據 ~_~ </div>@endif
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,84 +1,69 @@
+
 @extends('layouts.app')
-
 @section('content')
-
 <div class="container">
-    <div class="col-md-10 col-md-offset-1">
-        <div class="panel panel-default">
-            
-            <div class="panel-heading">
-                <h1>
-                    <i class="glyphicon glyphicon-edit"></i> Topic /
-                    @if($topic->id)
-                        Edit #{{$topic->id}}
-                    @else
-                        Create
-                    @endif
-                </h1>
+  <div class="col-md-10 col-md-offset-1">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h1><i class="glyphicon glyphicon-edit"></i>Topic /
+          @if($topic->id)
+          Edit #
+          {{$topic->id}}@else
+          Create
+          @endif
+        </h1>
+      </div>@include('common.error')
+      <div class="panel-body">@if($topic->id)
+        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
+          <input type="hidden" name="_method" value="PUT"/>@else
+          <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">@endif
+            <input type="hidden" name="_token" value="{{ csrf_token() }}"/>
+            <div class="form-group">
+              <label for="title-field">Title</label>
+              <input id="title-field" type="text" name="title" value="{{ old('title', $topic-&gt;title ) }}" class="form-control"/>
             </div>
-
-            @include('common.error')
-
-            <div class="panel-body">
-                @if($topic->id)
-                    <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-                        <input type="hidden" name="_method" value="PUT">
-                @else
-                    <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-                @endif
-
-                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                    
-                <div class="form-group">
-                	<label for="title-field">Title</label>
-                	<input class="form-control" type="text" name="title" id="title-field" value="{{ old('title', $topic->title ) }}" />
-                </div> 
-                <div class="form-group">
-                	<label for="body-field">Body</label>
-                	<textarea name="body" id="body-field" class="form-control" rows="3">{{ old('body', $topic->body ) }}</textarea>
-                </div> 
-                <div class="form-group">
-                    <label for="user_id-field">User_id</label>
-                    <input class="form-control" type="text" name="user_id" id="user_id-field" value="{{ old('user_id', $topic->user_id ) }}" />
-                </div> 
-                <div class="form-group">
-                    <label for="category_id-field">Category_id</label>
-                    <input class="form-control" type="text" name="category_id" id="category_id-field" value="{{ old('category_id', $topic->category_id ) }}" />
-                </div> 
-                <div class="form-group">
-                    <label for="reply_count-field">Reply_count</label>
-                    <input class="form-control" type="text" name="reply_count" id="reply_count-field" value="{{ old('reply_count', $topic->reply_count ) }}" />
-                </div> 
-                <div class="form-group">
-                    <label for="view_count-field">View_count</label>
-                    <input class="form-control" type="text" name="view_count" id="view_count-field" value="{{ old('view_count', $topic->view_count ) }}" />
-                </div> 
-                <div class="form-group">
-                    <label for="last_reply_user_id-field">Last_reply_user_id</label>
-                    <input class="form-control" type="text" name="last_reply_user_id" id="last_reply_user_id-field" value="{{ old('last_reply_user_id', $topic->last_reply_user_id ) }}" />
-                </div> 
-                <div class="form-group">
-                    <label for="order-field">Order</label>
-                    <input class="form-control" type="text" name="order" id="order-field" value="{{ old('order', $topic->order ) }}" />
-                </div> 
-                <div class="form-group">
-                	<label for="excerpt-field">Excerpt</label>
-                	<textarea name="excerpt" id="excerpt-field" class="form-control" rows="3">{{ old('excerpt', $topic->excerpt ) }}</textarea>
-                </div> 
-                <div class="form-group">
-                	<label for="slug-field">Slug</label>
-                	<input class="form-control" type="text" name="slug" id="slug-field" value="{{ old('slug', $topic->slug ) }}" />
-                </div>
-
-                    <div class="well well-sm">
-                        <button type="submit" class="btn btn-primary">Save</button>
-                        <a class="btn btn-link pull-right" href="{{ route('topics.index') }}"><i class="glyphicon glyphicon-backward"></i>  Back</a>
-                    </div>
-                </form>
+            <div class="form-group">
+              <label for="body-field">Body</label>
+              <textarea id="body-field" name="body" rows="3" class="form-control">{{ old('body', $topic->body ) }}</textarea>
             </div>
-        </div>
+            <div class="form-group">
+              <label for="user_id-field">User_id</label>
+              <input id="user_id-field" type="text" name="user_id" value="{{ old('user_id', $topic->user_id ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="category_id-field">Category_id</label>
+              <input id="category_id-field" type="text" name="category_id" value="{{ old('category_id', $topic->category_id ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="reply_count-field">Reply_count</label>
+              <input id="reply_count-field" type="text" name="reply_count" value="{{ old('reply_count', $topic-&gt;reply_count ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="view_count-field">View_count</label>
+              <input id="view_count-field" type="text" name="view_count" value="{{ old('view_count', $topic-&gt;view_count ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="last_reply_user_id-field">Last_reply_user_id</label>
+              <input id="last_reply_user_id-field" type="text" name="last_reply_user_id" value="{{ old('last_reply_user_id', $topic-&gt;last_reply_user_id ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="order-field">Order</label>
+              <input id="order-field" type="text" name="order" value="{{ old('order', $topic->order ) }}" class="form-control"/>
+            </div>
+            <div class="form-group">
+              <label for="excerpt-field">Excerpt</label>
+              <textarea id="excerpt-field" name="excerpt" rows="3" class="form-control">{{ old('excerpt', $topic->excerpt ) }}</textarea>
+            </div>
+            <div class="form-group">
+              <label for="slug-field">Slug</label>
+              <input id="slug-field" type="text" name="slug" value="{{ old('slug', $topic->slug ) }}" class="form-control"/>
+            </div>
+            <div class="well well-sm">
+              <button type="submit" class="btn btn-primary">Save</button><a href="{{ route('topics.index') }}" class="btn btn-link pull-right"><i class="glyphicon glyphicon-backward"></i>Back</a>
+            </div>
+          </form>
+        </form>
+      </div>
     </div>
-</div>
-
-@endsection
+  </div>
+</div>@endsection
