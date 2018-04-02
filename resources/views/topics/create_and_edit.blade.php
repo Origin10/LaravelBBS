5a88d4185dd2aa1ad82ebfb6a5b488b478698437
林傳哲 <w.crows@gmail.com>
2018-04-02 20:36:15 +0800

    User can upload avatar

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -39,21 +39,25 @@
   </div>
 </div>@endsection
 @section('styles')
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
-@stop
-
+<link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}"/>@stop
 @section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
-
-    <script>
-    $(document).ready(function(){
-        var editor = new Simditor({
-            textarea: $('#editor'),
-        });
-    });
-    </script>
-
-@stop
+<script type="text/javascript" src="{{ asset('js/module.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/hotkeys.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/uploader.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/simditor.js') }}"></script>
+<script>
+  $(document).ready(function(){
+  var editor = new Simditor({
+  textarea: $('#editor'),
+  upload: {
+                url: '{{ route('topics.upload_image') }}',
+                params: { _token: '{{ csrf_token() }}' },
+                fileKey: 'upload_file',
+                connectionCount: 3,
+                leaveConfirm: '文件上傳中，關閉此頁面將取消上傳。'
+            },
+            pasteImage: true,
+            });
+  });
+  
+</script>@stop
