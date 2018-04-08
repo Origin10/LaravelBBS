fd932cccd889e623657ac3cef553b7d859ce5897
林傳哲 <w.crows@gmail.com>
2018-04-08 15:06:11 +0800

    User can delete reply

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -15,21 +15,21 @@ class RepliesController extends Controller
         $this->middleware('auth');
     }
 
-	public function store(ReplyRequest $request, Reply $reply)
-	{
+    public function store(ReplyRequest $request, Reply $reply)
+    {
         $reply->content = $request->input('content');
         $reply->user_id = Auth::id();
         $reply->topic_id = $request->input('topic_id');
         $reply->save();
 
-		return redirect()->to($reply->topic->link())->with('success', '回复成功');
-	}
+        return redirect()->to($reply->topic->link())->with('success', '回复成功');
+    }
 
-	public function destroy(Reply $reply)
-	{
-		$this->authorize('destroy', $reply);
-		$reply->delete();
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('destroy', $reply);
+        $reply->delete();
 
-		return redirect()->route('replies.index')->with('succes', '删除成功');
-	}
+        return redirect()->to($reply->topic->link())->with('success', '成功刪除回覆！');
+    }
 }
--- resources/views/topics/_reply_list.blade.php
+++ resources/views/topics/_reply_list.blade.php
@@ -3,7 +3,14 @@
   <div name="reply{{ $reply->id }}" id="reply{{ $reply->id }}" class="media">
     <div class="avatar pull-left"><a href="{{ route('users.show', [$reply->user_id]) }}"><img alt="{{ $reply->user->name }}" src="{{ $reply->user->avatar }}" style="width:48px;height:48px;" class="media-object img-thumbnail"/></a></div>
     <div class="infos">
-      <div class="media-heading"><a href="{{ route('users.show', [$reply->user_id]) }}" title="{{ $reply->user->name }}">{{ $reply->user->name }}</a><span> •  </span><span title="{{ $reply->created_at }}" class="meta">{{ $reply->created_at->diffForHumans() }}</span>{{-- 回復刪除按鈕 --}}<span class="meta pull-right"><a title="删除回复"><span aria-hidden="true" class="glyphicon glyphicon-trash"></span></a></span></div>
+      <div class="media-heading"><a href="{{ route('users.show', [$reply->user_id]) }}" title="{{ $reply->user->name }}">{{ $reply->user->name }}</a><span> •  </span><span title="{{ $reply->created_at }}" class="meta">{{ $reply->created_at->diffForHumans() }}</span>{{-- 回復刪除按鈕 --}}
+        @can('destroy', $reply)<span class="meta pull-right">
+          <form action="{{ route('replies.destroy', $reply->id) }}" method="post">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button type="submit" class="btn btn-default btn-xs pull-left"><i class="glyphicon glyphicon-trash"></i></button>
+          </form></span>@endcan
+      </div>
       <div class="reply-content">{!! $reply->content !!}</div>
     </div>
   </div>
