54f1e4d8a0796501f36a38a01eda9610edc6cc76
林傳哲 <w.crows@gmail.com>
2018-04-08 14:34:52 +0800

    Notify user in email with Queue supported

--- resources/views/errors/419.blade.php
+++ resources/views/errors/419.blade.php
@@ -1,9 +1,41 @@
 @extends('errors::layout')
 
-@section('title', 'Page Expired')
+@section('title', '頁面過期')
 
 @section('message')
-    The page has expired due to inactivity.
-    <br/><br/>
-    Please refresh and try again.
+
+頁面有效期限已過期。
+<br/><br/>
+
+<form id="form1" runat="server">
+    <div id='div1'></div>
+    <span>秒後自動重新整理或者點擊&nbsp;</span>
+    <a href="#" onclick="javascript:window.location.reload()">重新整理</a>
+</form>
+
+
+<script type="text/javascript">
+
+//設定倒數秒數
+var t = 5;
+
+//顯示倒數秒收
+function showTime()
+{
+    t -= 1;
+    document.getElementById('div1').innerHTML= t;
+
+    if(t==0)
+    {
+      window.location.reload();
+    }
+
+    //每秒執行一次,showTime()
+    setTimeout("showTime()",1000);
+}
+
+//執行showTime()
+showTime();
+</script>
+
 @stop
