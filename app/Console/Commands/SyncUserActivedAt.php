d47a520ce4a5ceba570acc1841b309fce751fb9f
林傳哲 <w.crows@gmail.com>
2018-04-14 17:35:46 +0800

    Keep track of user last visited time

---
+++ app/Console/Commands/SyncUserActivedAt.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class SyncUserActivedAt extends Command
+{
+  protected $signature = 'larabbs:sync-user-actived-at';
+  protected $description = '將用戶最後登入時間從 Redis 同步在數計庫中';
+
+  public function handle(User $user)
+  {
+      $user->syncUserActivedAt();
+      $this->info("同步成功");
+  }
+}
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,56 +1,41 @@
+
 @extends('layouts.app')
-@section('title', "{$user->name} 的个人中心")
-
+@section('title', $user->name . ' 的個人中心')
 @section('content')
-    <div class="row">
-        <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="media">
-                        <div align="center">
-                            <img class="thumbnail img-responsive" src="{{ $user->avatar }}" width="300px" height="300px">
-                        </div>
-                        <div class="media-body">
-                            <hr>
-                            <h4><strong>个人简介</strong></h4>
-                            <p>{{ $user->introduction }}</p>
-                            <hr>
-                            <h4><strong>注册于</strong></h4>
-                            <p>{{ $user->created_at->diffForHumans() }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="row">
+  <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <div class="media">
+          <div align="center"><img src="{{ $user->avatar }}" width="300px" height="300px" class="thumbnail img-responsive"/></div>
+          <div class="media-body">
+            <hr/>
+            <h4><strong>個人簡介</strong></h4>
+            <p>{{ $user->introduction }} </p>
+            <hr/>
+            <h4><strong>註冊於</strong></h4>
+            <p>{{ $user->created_at->diffForHumans() }}</p>
+            <hr/>
+            <h4><strong>最後活躍</strong></h4>
+            <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
+          </div>
         </div>
-        <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                <span>
-                    <h1 class="panel-title pull-left" style="font-size:30px;">{{ $user->name }} <small>{{ $user->email }}</small></h1>
-                </span>
-                </div>
-            </div>
-            <hr>
-
-            {{-- 用户发布的内容 --}}
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <ul class="nav nav-tabs">
-                        <li class="{{ active_class(if_query('tab', null)) }}">
-                            <a href="{{ route('users.show', $user->id) }}">Ta 的话题</a>
-                        </li>
-                        <li class="{{ active_class(if_query('tab', 'replies')) }}">
-                            <a href="{{ route('users.show', [$user->id, 'tab' => 'replies']) }}">Ta 的回复</a>
-                        </li>
-                    </ul>
-                    @if (if_query('tab', 'replies'))
-                        @include('users._replies', ['replies' => $user->replies()->with('topic')->recent()->paginate(5)])
-                    @else
-                        @include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
-                    @endif
-                </div>
-            </div>
-            
-        </div>
+      </div>
     </div>
-@stop
+  </div>
+  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+    <div class="panel panel-default">
+      <div class="panel-body"><span>
+          <h1 style="font-size:30px;" class="panel-title pull-left">{{ $user->name }} <small>{{ $user->email }}</small></h1></span></div>
+    </div>
+    <hr/>{{-- 用戶發布的內容 --}}
+    <div class="panel panel-default">
+      <div class="panel-body">
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#">Ta 的話題</a></li>
+          <li><a href="#">Ta 的回覆</a></li>
+        </ul>@include('users._topics', ['topics' => $user->topics()->recent()->paginate(5)])
+      </div>
+    </div>
+  </div>
+</div>@stop
